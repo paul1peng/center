47203bb95b43cc9d7c6f8f7dbaa62514b995a3f7
Paul Peng <paul1peng@gmail.com>
2020-10-26 13:10:18 -0400

    Move initializing sketch into its own function.

--- center.js
+++ center.js
@@ -1,21 +1,24 @@
-var n = 127; // sym ==> 128; asym ==> 127
 var dm = 50;
-var s = 1;
-var ma = s * 256;
 
-var elements = [];
+var ma, circles;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  //background(0); // for contrast-y b&w
-  background(128); // for smooth b&w
   angleMode(RADIANS); 
+  colorMode(RGB);
+  initCenter();
+}
 
-  //colorMode(HSB, 255); background(0, 0, 128);
+function initCenter() {
+  var n = 127; // sym ==> 128; asym ==> 127
+  var s = 1;
+
+  ma = s * 256;
+  circles = [];
 
   for (var i = 0; i < n; i++) {
     var r = i / n * TWO_PI;
-    elements.push({
+    circles.push({
       x: width / 2,
       y: height / 2,
       dx: sin(r) * s,
@@ -28,31 +31,34 @@ function setup() {
       //v: random(255) // just fuck me up!!!
     });
   }
+
+  //background(0); // for contrast-y b&w
+  background(128); // for smooth b&w
 }
 
 function draw() {
+  var n = circles.length;
+
+  /* move each circle, wrapping around drawing surface as necessary */
   for (var i = 0; i < n; i++) {
-    var e = elements[i];
-    // for compositionally sound version
+    var e = circles[i];
     e.x = mod(e.x + e.dx, width); 
     e.y = mod(e.y + e.dy, height);
-    /*
-    // for cropped version
-    e.x = mod(e.x + e.dx + dm, width + dm * 2) - dm;
-    e.y = mod(e.y + e.dy + dm, height + dm * 2) - dm;
-    */
   }
+
+  /* draw line between overlapping circles */
   for (var i = 0; i < n; i++) {
-    var e1 = elements[i];
+    var e1 = circles[i];
     for (var j = 0; j < i; j++) {
-      var e2 = elements[j];
+      var e2 = circles[j];
       var d = dist(e1.x, e1.y, e2.x, e2.y);
-      if (d <= dm) {
+      if (d <= dm) { // if overlapping
         stroke(
+          /* average of the two circles' colors */
           (e1.r + e2.r) / 2,
           (e1.g + e2.g) / 2,
           (e1.b + e2.b) / 2,
-          //255, 255, // for HSB version
+          /* lighter when further, darker when closer */
           ma - d / dm * ma
         );
         line(e1.x, e1.y, e2.x, e2.y);
