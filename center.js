ecf83ccf6a5fd1037c5053cf2b2527dbf02e8ad2
Paul Peng <paul1peng@gmail.com>
2020-10-26 14:18:53 -0400

    Randomize angle and count; set up random color schemes.

--- center.js
+++ center.js
@@ -10,44 +10,67 @@ function setup() {
 }
 
 function initCenter() {
-  var n = 127; // sym ==> 128; asym ==> 127
-  var s = 1;
+  //var rseed = floor(random(0, 128));
+  // TODO choose ird between : horiz, vert, 45s, diags, recips
+  var ird = floor(random(0, 32));
+  var n = floor(random(112, 145));
+  var s = 1; // random(0.1875, 0.3125); 
 
-  ma = s * 256;
+
+  /*
+  var fv = random([
+    function(i) { return 255 * (i % 2); },
+    // 64 + 128 * (i % 2) // 32 + 192 * (i % 2)
+    function(i) { return i / n * 255; },
+    // i < n / 2 ? map(i, 0, n / 2, 255, 0): map (i, n/2, n, 0, 255)
+    // i < n / 2 ? 2 * i / n * 255 : 255 - (i - n / 2) / n * 255
+    // i < n / 2 ? 2 * i / n * 255 : 255 - 2 * (i % n / 2) / n * 255
+    function(i) { return random(255); },
+    function(i) { return noise(25 * i / n) * 255; }
+  ]);
+  */
+
+  ma = s * 192;
   circles = [];
 
+  var ir = TWO_PI * (ird / 32);
   for (var i = 0; i < n; i++) {
-    var r = i / n * TWO_PI;
+    var r = ir + i / n * TWO_PI;
     circles.push({
       x: width / 2,
       y: height / 2,
       dx: sin(r) * s,
       dy: cos(r) * s,
-      r: 255 * (i % 2),
-      g: 255 * (i % 2),
-      b: 255 * (i % 2)
-      //v: 255 * (i % 2) // for contrast-y b&w (works best with even n)
-      //v: i / n * 255 // for smooth b&w
-      //v: random(255) // just fuck me up!!!
+      v: i / n * 255
+      //v: i / n * 255
+      //r: 255 * (i % 2),
+      //g: 255 * (i % 2),
+      //b: 255 * (i % 2)
     });
   }
 
-  //background(0); // for contrast-y b&w
-  background(128); // for smooth b&w
+  background(128); // for contrast-y b&w
+  //background(128); // for smooth b&w
 }
 
 function draw() {
   var n = circles.length;
+  moveCircles();
+  drawCircleInteractions();
+}
 
-  /* move each circle, wrapping around drawing surface as necessary */
-  for (var i = 0; i < n; i++) {
+function moveCircles() {
+  /* move each circle, wrapping around drawing surface if necessary */
+  for (var i = 0; i < circles.length; i++) {
     var e = circles[i];
     e.x = mod(e.x + e.dx, width); 
     e.y = mod(e.y + e.dy, height);
   }
+}
 
+function drawCircleInteractions() {
   /* draw line between overlapping circles */
-  for (var i = 0; i < n; i++) {
+  for (var i = 0; i < circles.length; i++) {
     var e1 = circles[i];
     for (var j = 0; j < i; j++) {
       var e2 = circles[j];
@@ -55,21 +78,16 @@ function draw() {
       if (d <= dm) { // if overlapping
         stroke(
           /* average of the two circles' colors */
-          (e1.r + e2.r) / 2,
-          (e1.g + e2.g) / 2,
-          (e1.b + e2.b) / 2,
+          (e1.v + e2.v) / 2,
+          //(e1.r + e2.r) / 2,
+          //(e1.g + e2.g) / 2,
+          //(e1.b + e2.b) / 2,
           /* lighter when further, darker when closer */
           ma - d / dm * ma
         );
         line(e1.x, e1.y, e2.x, e2.y);
       }
     }
-  }
-}
-
-function keyTyped() {
-  if (key === 's') {
-    save('drawing.png');
   }
 }
 
